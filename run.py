b39914800bd27f61ebc21f9f179d9bba0b5c5e7a
kelllyy101 <>
2023-07-19 11:14:39 +0000

    refactor update functions into one

--- run.py
+++ run.py
@@ -57,17 +57,17 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+# def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
     """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+    #print("Updating sales worksheet...\n")
+    #sales_worksheet = SHEET.worksheet("sales")
+    #sales_worksheet.append_row(data)
+    #print("Sales worksheet updated successfully.\n")
 
 
-def update_surplus_worksheet(data):
+#def update_surplus_worksheet(data):
     """
     Update surplus worksheet, add new row with the list data provided
     """
@@ -75,6 +75,16 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
+
+#def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    #print(f"Updating {worksheet} worksheet...\n")
+    #worksheet_to_update = SHEET.worksheet(worksheet)
+    #worksheet_to_update.append_row(data)
+    #print(f"{worksheet} worksheet updated successfully\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -100,11 +110,14 @@ def main():
     """
     Run program functions
     """
+      """
+    Run all program functions
+    """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_row)
-    print(new_surplus_data)
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print('Welcome to Love Sandwiches!')
 main()
